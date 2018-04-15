ca740a8fac2539127545914a9a4da0969c5ac36d
Martin Allison <martinallison@gmail.com>
2018-04-15 11:52:45 +0100

    Release 0.0.4

---
+++ docs/js/components/listen.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <audio ref="audio"
+               :src="$g.getUrl('/audio/' + word + '.m4a')"
+               v-on:playing="playing = true"
+               v-on:ended="playing = false">
+        </audio>
+        <span v-if="playing">👂</span>
+        <a v-if="!playing" href="#" @click.prevent="$refs.audio.play()">
+            <img class="listen"
+                 :src="$g.getUrl('/img/audio.svg')"
+                 :alt="'listen to pronunciation of ' + word">
+        </a>
+    </div>
+</template>
+
+<script>
+    const Listen = {
+        name: "listen",
+        props: ["word"],
+        data: () => {
+            return {
+                playing: false
+            }
+        },
+    };
+
+    export default Listen;
+</script>
--- docs/js/main.js
+++ docs/js/main.js
@@ -1,45 +1,10 @@
-(function(document) {
-    /**
-     * Reeeaaaally rough way of wiring up the speaker buttons to playing the correct
-     * audio files.
-     */
-    var audioButtons = document.querySelectorAll("[data-play]");
-    var audioElements = document.querySelectorAll("audio");
+import Vue from "vue";
+import Listen from "./components/listen.vue";
+import globals from "./globals.js";
 
-    audioElements.forEach(function(el) {
-        /**
-         * For each audio element, if it has a corresponding button, replace it with
-         * an ear emoji while the audio plays, switching it back to the button when
-         * the audio is done.
-         */
-        var ear = document.createElement("span");
-        ear.innerText = "👂";
+Vue.use(globals);
+Vue.component("listen", Listen);
 
-        var button = document.querySelector("[data-play='" + el.id + "'");
-
-        if (!!button) {
-            el.addEventListener("playing", function() {
-                button.classList.add("hide");
-                ear = button.parentNode.appendChild(ear);
-            });
-            el.addEventListener("ended", function() {
-                button.classList.remove("hide");
-                ear = button.parentNode.removeChild(ear);
-            });
-        }
-    });
-
-    audioButtons.forEach(function(el) {
-        /**
-         * Hook up each 'button' to play the corresponding audio.
-         */
-        el.addEventListener("click", function(e) {
-            e.preventDefault();
-            var audio = document.getElementById(el.dataset.play);
-
-            if (!!audio) {
-                audio.play();
-            }
-        });
-    });
-}(document));
+const app = new Vue({
+    el: "#content",
+});
