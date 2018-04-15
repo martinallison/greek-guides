9d3dce89d7f9d8376202a628ae8ba9c047632182
Martin Allison <martinallison@gmail.com>
2018-04-15 01:44:47 +0100

    Move Vue listen component into its own SFC file. Move globals to its own file too

--- src/js/main.js
+++ src/js/main.js
@@ -1,48 +1,10 @@
 import Vue from "vue";
-
-
-window.urlPrefix = "";
-
-
-const globals = {
-    getUrl: path => window.urlPrefix + path
-}
-
-
-globals.install = function() {
-    Object.defineProperty(Vue.prototype, "$g", {
-        get() { return globals }
-    })
-}
-
-
-const Listen = {
-    name: "listen",
-    props: ["word"],
-    data: () => {
-        return {
-            playing: false
-        }
-    },
-    template: `
-        <div>
-            <audio ref="audio"
-                :src="$g.getUrl('/audio/' + word + '.m4a')"
-                v-on:playing="playing = true"
-                v-on:ended="playing = false">
-            </audio>
-            <span v-if="playing">👂</span>
-            <a v-if="!playing" href="#" @click.prevent="$refs.audio.play()">
-                <img :src="$g.getUrl('/img/audio.svg')" class="listen"
-                    :alt="'listen to pronunciation of ' + word">
-            </a>
-        </div>
-    `
-};
+import Listen from "./components/listen.vue";
+import globals from "./globals.js";
 
 Vue.use(globals);
 Vue.component("listen", Listen);
 
-new Vue({
+const app = new Vue({
     el: "#content",
 });
